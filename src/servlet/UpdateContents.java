06c851b8d18d9cbc5999fc14e09bbaa3d32013dd
syun-lips <syun-ilp@172.30.13.21>
2018-07-31 12:30:51 +0900

    写真を挿入時のエラー修正

--- src/servlet/UpdateContents.java
+++ src/servlet/UpdateContents.java
@@ -3,8 +3,11 @@ package servlet;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -19,6 +22,7 @@ import model.ContentsBean;
  * Servlet implementation class UpdateContents
  */
 @WebServlet("/UpdateContents")
+@MultipartConfig(maxFileSize=1048576)
 public class UpdateContents extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -58,18 +62,55 @@ public class UpdateContents extends HttpServlet {
 		ContentsDAO contentsdao = new ContentsDAO();
 		ContentsBean contentsbean = (ContentsBean)session.getAttribute("updatecontents");
 
-		String name = request.getParameter("name");
-		Date start = Date.valueOf(request.getParameter("start"));
-		Date end = Date.valueOf(request.getParameter("end"));
-		//画像挿入処理
-		InputStream is= null;
-		Part filePart = request.getPart("picture");
-		if (filePart != null) {
-			is = filePart.getInputStream();
-			contentsdao.contentsUpdate(contentsbean.getContentsID(), name, start, end, is);
-		}else{
-			contentsdao.contentsUpdateBlob(contentsbean.getContentsID(), name, start, end);
+		String name = request.getParameter("contentsName");
+		String str_start = request.getParameter("start");
+		String str_end = request.getParameter("end");
+		
+		Date start = null;
+		Date end = null;
+		try{
+			// String → java.util.Date → java.sql.Date
+						SimpleDateFormat sFormat = new SimpleDateFormat("yyyy-MM-dd");
+						
+
+						
+						// input type="date"の値から余計な文字列を削除
+						str_start = str_start.replace("T", " ");
+						str_end = str_end.replace("T", " ");
+						
+						java.util.Date util_start = new java.util.Date();
+						java.util.Date util_end = new java.util.Date();
+						
+						// String → java.util.Date
+						util_start = sFormat.parse(str_start);
+						util_end = sFormat.parse(str_end);
+						
+						// java.util.Date → java.sql.Date
+						Calendar calendar = Calendar.getInstance();
+						calendar.setTime(util_start);
+						calendar.set(Calendar.HOUR_OF_DAY, 0);
+						calendar.set(Calendar.MINUTE, 0);
+						calendar.set(Calendar.SECOND, 0);
+						calendar.set(Calendar.MILLISECOND, 0);
+						start = new java.sql.Date(calendar.getTimeInMillis());
+						
+						calendar.setTime(util_end);
+						end = new java.sql.Date(calendar.getTimeInMillis());
+						
+						//画像挿入処理
+						InputStream is= null;
+						Part filePart = request.getPart("picture");
+						if (filePart != null) {
+							is = filePart.getInputStream();
+							contentsdao.contentsUpdate(contentsbean.getContentsID(), name, start, end, is);
+						}else{
+							contentsdao.contentsUpdateBlob(contentsbean.getContentsID(), name, start, end);
+						}
+			
+		}catch (Exception e) {
+			System.out.println(e);
 		}
+		
 
 
 		request.getRequestDispatcher("GetContents").forward(request, response);
